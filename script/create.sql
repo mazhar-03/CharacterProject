9df50ac2ab83ff69979ae50a5e0a23b77224d458
Mazhar Altincay <mazharaltincay89@gmail.com>
2025-04-20 14:36:10 +0200

    Add scripts for database

---
+++ script/create.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE Warriors (
+                          Id NVARCHAR(10) PRIMARY KEY,
+                          Name NVARCHAR(50) NOT NULL,
+                          IsAlive BIT NOT NULL,
+                          Stamina INT NOT NULL
+);
+GO
+
+CREATE TABLE Mages (
+                       Id NVARCHAR(10) PRIMARY KEY,
+                       Name NVARCHAR(50) NOT NULL,
+                       IsAlive BIT NOT NULL,
+                       Mana INT NULL
+);
+GO
+
+CREATE TABLE Rogues (
+                        Id NVARCHAR(10) PRIMARY KEY,
+                        Name NVARCHAR(50) NOT NULL,
+                        IsAlive BIT NOT NULL,
+                        StealthCode NVARCHAR(20) NOT NULL,
+                        GuildName NVARCHAR(100) NOT NULL
+);
+GO
---
+++ script/insert.sql
@@ -0,0 +1,17 @@
+﻿INSERT INTO Warriors (Id, Name, IsAlive, Stamina) VALUES
+    ('W-1', 'Garen', 0, 85),
+    ('W-2', 'Darius', 1, 10),
+    ('W-3', 'Pantheon', 1, 5);
+GO
+
+INSERT INTO Mages (Id, Name, IsAlive, Mana) VALUES
+    ('M-1', 'Veigar', 0, NULL),
+    ('M-2', 'Lux', 1, 100),
+    ('M-3', 'Brand', 1, 30);
+GO
+
+INSERT INTO Rogues (Id, Name, IsAlive, StealthCode, GuildName) VALUES
+    ('R-1', 'Talon', 0, 'RG-1001', 'Black Fang'),
+    ('R-2', 'Zed', 1, 'RG-2002', 'Shadow Clan'),
+    ('R-3', 'Evelynn', 1, 'WRONGCODE', 'Black Fang');
+GO
